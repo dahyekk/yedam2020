81bb78de497c659187fc185ac68c0a7cb9916a4c
User <User@YD02-05>
2020-01-30 09:07:36 +0900

    t

---
+++ HelloWorld/src/referenceType/CalculatorExample.java
@@ -0,0 +1,53 @@
+package referenceType;
+
+public class CalculatorExample {
+	public static void main(String[] args) {
+		int num1 = 10;
+		int num2 = 20;
+		sum (num1, num2);
+		minus(num1,num2);
+		multi(55, 4);
+		divide(10,7);
+		print(7,"*");
+		printStar(10,"+");
+		int r = sum2(4,7);
+		minus(r,7);
+		
+	}
+	// int -> 반환값 == int
+	public static int sum2(int a , int b) {
+		int result ;
+		result = a+b;
+		return result;
+	}
+	//void ->  반환값 없음
+	public static void print(int a, String str) {
+		for (int i = 0; i < a; i++) {
+			System.out.println(str);
+		}
+	}
+	public static void printStar(int a, String s) {
+		for (int i = 0; i <a; i ++) {
+			for (int j = 0; j<=i; j++) {
+				System.out.print(s);
+			}
+			System.out.println();
+		}
+	}
+	public static void sum(int a, int b) {
+		int result = a+b;
+		System.out.println("두수의 합은 : "+result);
+	}
+	public static void minus(int x, int y) {
+		int result = x-y;
+		System.out.println("두수의 차이는 : "+result);
+	}
+	public static void multi(int x, int y) {
+		int result = x * y;
+		System.out.println("두수의 곱은 : "+result);
+	}
+	public static void divide(int x, int y ) {
+		double result = (double) x / y;
+		System.out.println("두수를 나누면 : "+ result);
+	}
+}
---
+++ HelloWorld/src/referenceType/ReferencdTypeExample.java
@@ -0,0 +1,31 @@
+package referenceType;
+
+class Strong{
+	int f1;
+	String str;
+	void print(){
+		
+	}
+}
+public class ReferencdTypeExample {
+	public static void main(String[] args) {
+		int num1 = 10;
+		byte by1 = 10;
+		by1 = (byte)num1;
+		
+		String str1 = new String("Hello");
+		String str2 = new String("Hello");
+		System.out.println(str1.equals(str2));
+		
+//		Strong str = new Strong();
+//		Strong str2 = new Strong();
+//		str.f1=10;
+//		str.str = "hello";
+//		//위치값
+//		System.out.println(str);
+//		//안에 담긴값
+//		System.out.println(str.f1);
+//		System.out.println(str.str);
+		
+	}
+}
